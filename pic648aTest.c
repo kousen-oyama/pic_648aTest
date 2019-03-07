4a14511cda884647dda9654e809ec4d31b719265
kino-n1851 <46987400+kino-n1851@users.noreply.github.com>
2019-03-08 01:50:12 +0900

    Update pic648aTest.c

--- pic648aTest.c
+++ pic648aTest.c
@@ -1,41 +1,41 @@
 
-//RB1�|�[�g�ɂ�L�`�J���s���v���O�����B
-//�v���W�F�N�g���쐬����ہA���{��ŃR�����g���c������
-//encoding��Shift_Jis�ɐݒ肷�邱�ƁB����ȊO���ƕ�����������B
+//RB1ポートにてLチカを行うプログラム。
+//プロジェクトを作成する際、日本語でコメントを残すため
+//encodingはShift_Jisに設定すること。これ以外だと文字化けする。
 
 // CONFIG
-//�ȉ��̃R���t�B�O�̓\�[�X�t�@�C���쐬��A
-//production �^�u��� set configuration bits��I���A
-//generate source code to Output���玩�������������́B
+//以下のコンフィグはソースファイル作成後、
+//production タブより set configuration bitsを選択、
+//generate source code to Outputから自動生成したもの。
 
-#pragma config FOSC = INTOSCIO //����p�N���b�N�̑I���BINTOSCIO�œ����N���b�N���g�p�\
-#pragma config WDTE = OFF      //�E�H�b�`�h�b�O�^�C�}�̎g�p�ہB��₱�����̂Ŏg��Ȃ��B 
-#pragma config PWRTE = ON      //�d�������シ���ɂ͓d�������肵�Ȃ����ꂪ����̂ŏ����҂B���D�݂ŁB
+#pragma config FOSC = INTOSCIO //動作用クロックの選択。INTOSCIOで内部クロックを使用可能
+#pragma config WDTE = OFF      //ウォッチドッグタイマの使用可否。ややこしいので使わない。 
+#pragma config PWRTE = ON      //電源投入後すぐには電圧が安定しない恐れがあるので少し待つ。お好みで。
 
-#pragma config MCLRE = ON      //MCLR�s�����n�[�h�E�F�A���Z�b�g�Ƃ��Ďg�����ǂ����B
-                               //ON�ɂ����ꍇ�AMCLR�s����GPIO�Ƃ��Ďg�p���邱�Ƃ͕s�\�ɂȂ�B
-                               //���10k���قǂŃv���A�b�v���Ă����B0V�ɗ��Ƃ��ƁA���Z�b�g�����B
+#pragma config MCLRE = ON      //MCLRピンをハードウェアリセットとして使うかどうか。
+                               //ONにした場合、MCLRピンをGPIOとして使用することは不可能になる。
+                               //常に10kΩほどでプルアップしておく。0Vに落とすと、リセットされる。
 
-#pragma config BOREN = ON      //�d�����s����ɂȂ����ۃ��Z�b�g���邩�ǂ����B���D�݁B
-#pragma config LVP = ON        //��d���ŏ������݂����B���ɂ͂���Ȃ��B
-#pragma config CPD = OFF       //�R�[�h�̕ی�Ƃ�����Ȃ́B�����B 
-#pragma config CP = OFF        //��Ɠ����悤�Ȃ́B�����
+#pragma config BOREN = ON      //電圧が不安定になった際リセットするかどうか。お好み。
+#pragma config LVP = ON        //低電圧で書き込みするやつ。自分にはいらない。
+#pragma config CPD = OFF       //コードの保護とかそんなの。しらん。 
+#pragma config CP = OFF        //上と同じようなの。いらね
 
 
 #include <xc.h>
-#define _XTAL_FREQ 4000000 //�f�t�H���g�ł͎��g����4MHz
+#define _XTAL_FREQ 4000000 //648aの場合、デフォルトでは周波数が4MHz
 
 
 int main(void)
 {
     
-    CMCON = 0x07;       //�R���p���[�^�@�\�̐ݒ�B�s�g�p��0x07
-    TRISA = 0b11000011; //���o�͂̐ݒ�B�|�[�g���B���bit����RA7->RA0�ɑΉ�
-    TRISB = 0x00;       //����������o�́B0x00=0b00000000 0=�o�� 1=����
+    CMCON = 0x07;       //コンパレータ機能の設定。不使用は0x07
+    TRISA = 0b11000011; //入出力の設定。ポート毎。上位bitからRA7->RA0に対応
+    TRISB = 0x00;       //こちらも入出力。RB7->RB0 0x00=0b00000000 0=出力 1=入力
     while (1){
-      //PORTB = 0b00000010;        //���̂悤�ɁA�S�̂��ꊇ�ŊǗ��\�B
-        RB1 = 1;                   //��Ɠ������Ƃ����Ă���
-        __delay_ms(200);           //�i���j�擪��__�́A�A���_�[�o�[2�B()���ɕϐ����������Ƃ͂ł��Ȃ��B
+      //PORTB = 0b00000010;        //このように、全体を一括で管理可能。
+        RB1 = 1;                   //上と同じことをしている
+        __delay_ms(200);           //（注）先頭の__は、アンダーバー2つ。()内に変数をつかうことはできない。
       //PORTB = 0b00000000;
         RB1 = 0;
         __delay_ms(200);
@@ -44,7 +44,7 @@ int main(void)
 }
 
 
-//�������݂̍ۂɂ́A�v���W�F�N�g���E�N���b�N����v���p�e�B�A
-//categories���pickit3��I�����AOption categories��Power�ɁA
-//��Ƀ`�F�b�N�����A�d����5V������(����4.75�ŗ��p��)�ɂ��邱�ƁB
+//書き込みの際には、プロジェクト名右クリックからプロパティ、
+//categoriesよりpickit3を選択し、Option categoriesをPowerに、
+//上にチェックを入れ、電圧を5Vあたり(自分は4.75で利用中)にすること。
 
